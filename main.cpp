4c6051d7f3f4e89cc2532c3ca44e39d46dea0d72
Matt Dietrich <MatthewDietrich@my.unt.edu>
2018-09-13 13:18:30 -0500

    Use C++ headers where possible

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 // For testing purposes
 
 #include "pwm-weather.h"
-#include <stdio.h>
+#include <iostream>
 
 int main()
 {
     WeatherDataAsPWMValues wd;
 
     convert_humidity_to_pwm(50.1f, &wd);
-    printf("%d\n", wd.temperature);
+    int a = wd.temperature;
+    std::cout << a << std::endl;
 
     return 0;
 }
